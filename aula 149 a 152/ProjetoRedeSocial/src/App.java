21d7e846074b04f7ebb0f424a3db164507a95dbd
daniel376116 <danielosilva2010@gmail.com>
2026-06-09 16:54:03 -0300

    Add files via upload

---
+++ aula 149 a 152/ProjetoRedeSocial/src/App.java
@@ -0,0 +1,17 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Usuario Usuario1 = new Usuario();
+		
+		Usuario1.nome = "Ana";
+		Usuario1.idade = 17;
+		Usuario1.cidade = "Valença";
+		
+		Usuario1.mostrarPerfil();
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ aula 149 a 152/ProjetoRedeSocial/src/Usuario.java
@@ -0,0 +1,15 @@
+
+public class Usuario {
+	
+	String nome, cidade;
+	int idade;
+	
+	void mostrarPerfil() {
+	 System.out.printf("Nome: %s%n", nome);
+	 System.out.printf("Idade: %d%n", idade);
+	 System.out.printf("Cidade: %s", cidade);
+	 
+		
+	}
+
+}
